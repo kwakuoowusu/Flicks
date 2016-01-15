2f43ede9544e285c05574b1d53a589ce939272a3
kwakuoowusu <kwakuoowusu@gmail.com>
2016-01-15 16:34:33 -0500

    added network requests to movies database api in order to see popular movies

--- MovieViewer/MovieViewer/MoviesViewController.swift
+++ MovieViewer/MovieViewer/MoviesViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         // Do any additional setup after loading the view.
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            //reload to fill out tables
+                            self.tableView.reloadData()
+                        
+                    }
+                }
+        });
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +50,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 20
+        //return same number of rows as movies given by movies database
+        //if movies does not exist return 0
+        if let movies = movies{
+            return movies.count
+        }else{
+            return 0
+        }
         
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
@@ -32,8 +64,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         //cells inside a table this allows the app not to load all cells at once, when a cell dissapears a new one is loaded
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell",forIndexPath: indexPath)
         
-        //debugging just to show cells
-        cell.textLabel!.text = "row \(indexPath.row)"
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        
+        cell.textLabel!.text = title
         print("row \(indexPath.row)")
         return cell
     }
